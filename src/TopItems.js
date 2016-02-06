3f84b85e5524743a35237116b3e8ba9fdf3d1e2d
Dan Bartlett <danbartlett@gmail.com>
2016-02-06 12:19:59 +0000

    Remove sort button, as we sort by default now

--- src/TopItems.js
+++ src/TopItems.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import Item from './Item';
-import sortByScore from './sorter';
 
 var TopItems = React.createClass({
-  sortByScore: function() {
-    this.setState({items: sortByScore(this.props.items)});
-  },
-
   render: function() {
     var rank = 0;
     var all_items = this.props.items.map(function(item) {
@@ -16,23 +11,11 @@ var TopItems = React.createClass({
       );
     });
 
-    // Retired, sort button
-    // <SortButton items={this.props.items} sortByScore={this.sortByScore}/>
     return (
       <div>
         <div className="top-stories">
           {all_items}
         </div>
-      </div>
-    );
-  }
-});
-
-var SortButton = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <a className='button' onClick={this.props.sortByScore}>Sort!</a>
       </div>
     );
   }
